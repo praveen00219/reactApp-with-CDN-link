fcc8098d662e46cf8822ce21dabffcadadd14168
praveen00219 <paru2192000@gmail.com>
2024-09-13 23:57:16 +0530

    modify UI

--- assets/script.js
+++ assets/script.js
@@ -16,38 +16,58 @@
 
 const MyReactApp = () => {
   // React functional Component
-  let name = "John Smith";
-  let age = 50;
-  let address = `123, ABC Street, NJ`;
+  let developerName = "Jordan Walke";
+  let releaseDate = 2013;
   return (
-    <div className="">
-      <h1 id="myheading" className="color-blue text-center ">
+    <div className="w-9/12 mx-auto">
+      <h1
+        id="myheading"
+        className="mx-auto mt-24 bg-gray-100 p-3 rounded-lg text-3xl font-bold text-gray-700 text-center "
+      >
         This is my first React App
       </h1>
 
-      <div className="card-container">
-        {/* first card */}
-        <div className="card text-center">
-          <img src="/assets/img/avatar-image.png" />
-          <h2>Name: {name}</h2>
-          <span>Age: {age}</span>
-          <br />
-          <span>Address: {address}</span>
-        </div>
-
-        {/* second card */}
-        <div className="card text-center">
-          <img src="/assets/img/avatar-image-2.png" />
-          <h2>Name: {name}</h2>
-          <span>Age: {age}</span>
-          <br />
-          <span>Address: {address}</span>
-        </div>
+      {/* first div with p tag */}
+      <div className="mx-auto mt-8 bg-gray-50 rounded-lg flex items-center justify-center">
+        <p className="text-center w-[600px]">
+          I created a straight forward web page using ReactJS with the
+          assistance of Content Delivery Network (CDN) links.
+        </p>
       </div>
-      <p className="text-center">
-        I created a straightforward web page using ReactJS with the assistance
-        of <br /> Content Delivery Network (CDN) links.
-      </p>
+
+      {/* second div with p and anchor tags */}
+      <div className=" mx-auto my-8  bg-gray-50 rounded-lg flex flex-col items-center justify-center">
+        <p className="text-center w-[600px] mb-5 text-gray-800">
+          ReactJS is a powerful JavaScript library that allows us to build
+          interactive and dynamic user interfaces. By leveraging CDNs, we can
+          quickly access the necessary React libraries without the need for
+          complex development setups. Use the React.createElement to create the
+          Element And for rendering it use React.render.
+        </p>
+
+        <span className="font-semibold">
+          Visit here : {/* anchor tag */}
+          <a
+            className="text-blue-500 hover:underline"
+            href="https://react.dev/"
+          >
+            React.dev
+          </a>
+        </span>
+      </div>
+
+      <hr></hr>
+
+      {/* 3rd div with p tag */}
+      <div className="mx-auto my-8 text-gray-700 font-semibold bg-gray-50 rounded-lg flex flex-col items-center justify-center">
+        <img
+          src="/assets/img/React_(web_framework)-Logo.svg"
+          className="logo"
+        />
+        <span>Release : {releaseDate}</span>
+        <h2>Developer : {developerName}</h2>
+        <p>Maintained and developed by Facebook.</p>
+      </div>
     </div>
   );
 };
